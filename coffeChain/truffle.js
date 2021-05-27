a74aefc29e7e1ccd5243516546a5c29a833045c5
samuelecisaro <samuele.cisaro@hotmail.it>
2021-05-27 08:13:17 +0200

    Add all project

---
+++ coffeChain/truffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+  compilers: {
+    solc: {
+      version: "^0.4.24", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+      parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+    }
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
