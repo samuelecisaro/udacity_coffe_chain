28e8986a481d2f4cce8dabb7728db5dd89f9219a
samuelecisaro <samuele.cisaro@hotmail.it>
2021-05-29 10:44:33 +0200

    feat: add all changes

--- coffeChain/truffle.js
+++ coffeChain/truffle.js
@@ -1,3 +1,11 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "";
+//
+// const fs = require('fs');
+const mnemonic = "";
+
+
+
 module.exports = {
   compilers: {
     solc: {
@@ -11,6 +19,15 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000,
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   }
 };
